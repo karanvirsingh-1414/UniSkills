f5271b811f59a3cceedba97dc159f636744da6ea
ishneet307 <ishneet307@gmail.com>
2026-03-25 11:24:09 +0530

    added Session model and controller logic

---
+++ models/Session.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Session = sequelize.define('Session', {
+  topic: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mentorId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  studentId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  creditsTransferred: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'pending' // 'pending', 'active', 'completed'
+  }
+});
+
+module.exports = Session;
