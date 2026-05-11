1defb6d1b080c54232c50b5a1a71d960f4013b29
karanvir singh <karanvirsingh141414@gmail.com>
2026-05-11 20:10:05 +0530

    Secured Firebase keys for website

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,15 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccount.json');
+let serviceAccount;
+
+try {
+    serviceAccount = require('./firebaseServiceAccount.json');
+} catch (error) {
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+        serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    } else {
+        throw new Error("Firebase Service Account not found. Add it to .env or create firebaseServiceAccount.json");
+    }
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
